310dddf618eec3d5d5ab947fd3dbfe85647d5160
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-02 01:57:34 -0300

    Requisito 8

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
   constructor() {
@@ -7,6 +8,8 @@ class MusicCard extends React.Component {
     this.listaMusicasNaTela = this.listaMusicasNaTela.bind(this);
     this.state = {
       novoArraydeMusicas: [],
+      loading: false,
+      checked: [],
     };
   }
 
@@ -23,25 +26,69 @@ class MusicCard extends React.Component {
     this.setState({
       novoArraydeMusicas: [...NovoArray],
     });
+    for (let i = 0; i < NovoArray.length; i += 1) {
+      this.setState((estadoAnterior) => ({
+        checked: [...estadoAnterior.checked, false],
+      }));
+    }
+  }
+
+  async adicionaMusicaAosFavoritos(event, trackId) {
+    const id = trackId;
+    const { novoArraydeMusicas, checked } = this.state;
+    const filtraMusica = novoArraydeMusicas.filter((element) => element.trackId === id);
+    this.setState({
+      loading: true,
+    });
+    await addSong(filtraMusica);
+    const index = event.target.id;
+    const checkeds = checked;
+    checkeds[index] = true;
+    this.setState({
+      loading: false,
+      checked: checkeds,
+    });
   }
 
   render() {
-    // const { listaDeMusicas } = this.props;
-    const { novoArraydeMusicas } = this.state;
+    const { novoArraydeMusicas, loading, checked } = this.state;
     return (
       <div>
-        {novoArraydeMusicas.map((element, i) => (
-          <div key={ i }>
-            <p key={ i }>{element.trackName}</p>
-            <audio data-testid="audio-component" src={ element.previewUrl } controls>
-              <track kind="captions" />
-              O seu navegador não suporta o elemento
-              {' '}
-              <code>audio</code>
-              .
-            </audio>
+        {loading ? <p>Carregando...</p> : (
+          <div>
+            {novoArraydeMusicas.map((element, i) => (
+              <div key={ i }>
+                <p key={ i }>{element.trackName}</p>
+                <audio
+                  data-testid="audio-component"
+                  src={ element.previewUrl }
+                  controls
+                >
+                  <track kind="captions" />
+                  O seu navegador não suporta o elemento
+                  {' '}
+                  <code>audio</code>
+                  .
+                </audio>
+                <label
+                  htmlFor="label"
+                >
+                  Favorita
+                  <input
+                    type="checkbox"
+                    id={ `${i}` }
+                    defaultChecked={ checked[i] }
+                    data-testid={ `checkbox-music-${element.trackId}` }
+                    onClick={
+                      (event) => this.adicionaMusicaAosFavoritos(event, element.trackId)
+                    }
+                  />
+                </label>
+              </div>
+            ))}
           </div>
-        ))}
+        )}
+
       </div>
     );
   }
