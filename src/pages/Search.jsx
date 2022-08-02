e2de355c51e91546b1e2bf9ebafd28384ba0b2c6
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-01 21:36:52 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -7,16 +9,24 @@ class Search extends React.Component {
     this.state = {
       music: '',
       disabled: true,
+      loading: false,
+      albunsArtistaPesquisado: [],
+      notAlbum: false,
+      artista: '',
     };
+    this.saveMusic = this.saveMusic.bind(this);
     this.searchMusic = this.searchMusic.bind(this);
+    this.listaAlbuns = this.listaAlbuns.bind(this);
+    this.exibeNomeArtista = this.exibeNomeArtista.bind(this);
   }
 
-  searchMusic(event) {
+  saveMusic(event) {
     const { target } = event;
     const { value } = target;
     const number = 2;
     this.setState({
       music: value,
+      artista: value,
     });
     if (value.length >= number) {
       this.setState({
@@ -30,25 +40,89 @@ class Search extends React.Component {
     }
   }
 
+  async searchMusic(event) {
+    const { music } = this.state;
+    event.preventDefault();
+    this.setState({
+      music: '',
+    });
+    this.setState({
+      loading: true,
+    });
+    const artistMusic = await searchAlbumsAPI(music);
+    this.setState({
+      albunsArtistaPesquisado: [...artistMusic],
+      loading: false,
+    });
+    if (artistMusic[0] === undefined) {
+      this.setState({
+        notAlbum: true,
+      });
+    }
+    if (artistMusic[0] !== undefined) {
+      this.setState({
+        notAlbum: false,
+      });
+    }
+  }
+
+  listaAlbuns() {
+    const { albunsArtistaPesquisado } = this.state;
+    if (albunsArtistaPesquisado[0] === undefined) {
+      return;
+    }
+    if (albunsArtistaPesquisado[0] !== undefined) {
+      return albunsArtistaPesquisado.map((element, index) => (
+        <div key={ index }>
+          <img src={ element.artworkUrl100 } alt={ element.artistName } />
+          <p>{element.collectionName}</p>
+          <p>{element.artistName}</p>
+          <Link
+            to={ `/album/${element.collectionId}` }
+            data-testid={ `link-to-album-${element.collectionId}` }
+          >
+            Album Completo
+          </Link>
+        </div>
+      ));
+    }
+  }
+
+  exibeNomeArtista() {
+    const { albunsArtistaPesquisado, artista } = this.state;
+    if (albunsArtistaPesquisado[0] === undefined) {
+      return;
+    }
+    if (albunsArtistaPesquisado[0] !== undefined) {
+      return <p>{`Resultado de álbuns de: ${artista}`}</p>;
+    }
+  }
+
   render() {
-    const { disabled, music } = this.state;
+    const { disabled, music, loading, notAlbum } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            data-testid="search-artist-input"
-            value={ music }
-            onChange={ this.searchMusic }
-          />
-          <button
-            type="submit"
-            disabled={ disabled }
-            data-testid="search-artist-button"
-          >
-            Pesquisar
-          </button>
-        </form>
+        {loading ? <p>Carregando...</p> : (
+          <form>
+            <input
+              data-testid="search-artist-input"
+              value={ music }
+              onChange={ this.saveMusic }
+            />
+            <button
+              type="submit"
+              disabled={ disabled }
+              data-testid="search-artist-button"
+              onClick={ this.searchMusic }
+            >
+              Pesquisar
+            </button>
+          </form>
+
+        )}
+        {notAlbum ? null : this.exibeNomeArtista()}
+        {notAlbum ? <p>Nenhum álbum foi encontrado</p> : this.listaAlbuns()}
       </div>
     );
   }
